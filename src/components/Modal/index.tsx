03820027af80a88267940c8b652d37a229c17cc3
@Kuva968 <kudelskiyv@gmail.com>
2023-09-25 11:42:38 +0300

    add ios

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,30 +1,29 @@
-import React, {useState} from 'react';
-import {Modal, StyleSheet, Text, View} from 'react-native';
-import Colors from '../../constants/Colors';
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
-import {useToast} from 'react-native-toast-notifications';
-import {Button} from 'react-native-paper';
-import {cameraOptions, photoLibraryOptions} from '../../constants/options';
+import React, { useState } from "react";
+import { Modal, StyleSheet, Text, View } from "react-native";
+import Colors from "../../constants/Colors";
+import { launchCamera, launchImageLibrary } from "react-native-image-picker";
+import { useToast } from "react-native-toast-notifications";
+import { Button } from "react-native-paper";
+import { cameraOptions, photoLibraryOptions } from "../../constants/options";
 interface IInfoModal {
   sendData: (uris: string[]) => void;
   close: () => void;
   isVisible: boolean;
 }
-const InfoModal = ({sendData, close, isVisible}: IInfoModal) => {
+const InfoModal = ({ sendData, close, isVisible }: IInfoModal) => {
   const toast = useToast();
   const [loading, setLoading] = useState(false);
 
   const choosePicture = async () => {
     setLoading(true);
-    launchImageLibrary(photoLibraryOptions, response => {
+    launchImageLibrary(photoLibraryOptions, (response) => {
       if (response.didCancel) {
         setLoading(false);
-        close();
-        toast.show(response.errorMessage ?? 'Error Choose Picture', {
-          type: 'danger',
-          placement: 'top',
+        toast.show(response.errorMessage ?? "Error Choose Picture", {
+          type: "danger",
+          placement: "top",
           duration: 4000,
-          animationType: 'zoom-in',
+          animationType: "zoom-in",
         });
       } else if (!response.didCancel && response.assets) {
         const responseUris = response?.assets.reduce(
@@ -35,7 +34,7 @@ const InfoModal = ({sendData, close, isVisible}: IInfoModal) => {
               return acc;
             }
           },
-          [],
+          []
         );
 
         setTimeout(() => {
@@ -43,11 +42,11 @@ const InfoModal = ({sendData, close, isVisible}: IInfoModal) => {
         }, 0);
         setTimeout(() => {
           toast.show(`Send ${responseUris.length} files `, {
-            type: 'success',
-            placement: 'top',
+            type: "success",
+            placement: "top",
             duration: 4000,
             //offset: 30,
-            animationType: 'zoom-in',
+            animationType: "zoom-in",
           });
         }, 700);
       }
@@ -56,9 +55,8 @@ const InfoModal = ({sendData, close, isVisible}: IInfoModal) => {
   };
 
   const takePicture = async () => {
-    close();
     launchCamera(cameraOptions)
-      .then(response => {
+      .then((response) => {
         if (
           response &&
           !response?.errorCode &&
@@ -69,25 +67,24 @@ const InfoModal = ({sendData, close, isVisible}: IInfoModal) => {
             sendData([response.assets[0].uri]);
           }, 0);
           setTimeout(() => {
-            toast.show('Send  file ', {
-              type: 'success',
-              placement: 'top',
+            toast.show("Send  file ", {
+              type: "success",
+              placement: "top",
               duration: 4000,
               //offset: 30,
-              animationType: 'zoom-in',
+              animationType: "zoom-in",
             });
           }, 700);
         } else {
-          close();
-          toast.show(response.errorMessage ?? 'Error take Photo', {
-            type: 'danger',
-            placement: 'top',
+          toast.show(response.errorMessage ?? "Error take Photo", {
+            type: "danger",
+            placement: "top",
             duration: 4000,
-            animationType: 'zoom-in',
+            animationType: "zoom-in",
           });
         }
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   };
 
   return (
@@ -95,14 +92,16 @@ const InfoModal = ({sendData, close, isVisible}: IInfoModal) => {
       animationType="slide"
       presentationStyle="formSheet"
       onRequestClose={close}
-      visible={isVisible}>
+      visible={isVisible}
+    >
       <View style={[styles.container]}>
         <Text style={styles.text}>Choose Media</Text>
         <Button
           style={styles.btn}
           mode="elevated"
           onPress={takePicture}
-          textColor={Colors.orange}>
+          textColor={Colors.orange}
+        >
           Take photo
         </Button>
 
@@ -112,7 +111,8 @@ const InfoModal = ({sendData, close, isVisible}: IInfoModal) => {
           style={styles.btn}
           mode="elevated"
           onPress={choosePicture}
-          textColor={Colors.backgroundColor}>
+          textColor={Colors.backgroundColor}
+        >
           Choose from library
         </Button>
       </View>
@@ -125,18 +125,18 @@ export default InfoModal;
 const styles = StyleSheet.create({
   mainBlock: {
     margin: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   container: {
     backgroundColor: Colors.backgroundColor,
-    alignItems: 'center',
+    alignItems: "center",
     gap: 60,
   },
   text: {
     fontSize: 24,
     color: Colors.textBrownColor,
-    textAlign: 'center',
+    textAlign: "center",
     marginTop: 40,
     marginBottom: 300,
   },
